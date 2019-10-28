c60d55fa2228eb3409a319dddaf5e2b4e7a891b0
robbie <robbiekruszynski@gmail.com>
2019-10-28 14:08:04 -0700

    reinstall yarn

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Test
+          Edit <code>src/App.js</code> and save to reload. TestING
         </p>
         <a
           className="App-link"
