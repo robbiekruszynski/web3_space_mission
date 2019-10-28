fea4350b1587045fb4d1961128a9a59316f0a5f3
robbie <robbiekruszynski@gmail.com>
2019-10-28 14:27:30 -0700

    start to port over styles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import logo from "./logo.svg";
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="button">
+        <button class="animated fadeIn delay-2s">Enter</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
