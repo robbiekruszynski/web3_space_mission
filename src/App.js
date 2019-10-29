54c6bd580f2ebf881ccc93f292ea1cec0752bf41
robbie <robbiekruszynski@gmail.com>
2019-10-29 15:58:14 -0700

    add work to splash page

--- src/App.js
+++ src/App.js
@@ -4,20 +4,29 @@ import Body from "./Body.js";
 import "./App.scss";
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      enterDiv: <div className="button"><button onClick={() => this.clickCheck()} className="animated fadeIn delay-2s">Enter</button></div>
-    }
+      enterDiv: (
+        <div className="button">
+          <button
+            onClick={() => this.clickCheck()}
+            className="animated fadeIn delay-2s"
+          >
+            Enter
+          </button>
+        </div>
+      )
+    };
   }
 
   clickCheck() {
-    this.setState({enterDiv: <Body>Loading...</Body>});
+    this.setState({ enterDiv: <Body>Loading...</Body> });
   }
   render() {
     return (
-      <div className="App">
+      <div className="Splash">
+        <div className="title">Web3 StarLab</div>
         <div>{this.state.enterDiv}</div>
       </div>
     );
