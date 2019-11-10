62ece9c004c13ccbda63549563a1c6bbb6ee4766
robbie <robbiekruszynski@gmail.com>
2019-11-10 04:03:25 -0800

    rework text section

--- src/GameOver.js
+++ src/GameOver.js
@@ -11,7 +11,8 @@ class GameOver extends React.Component {
       output =
         "The engines rumble, and you ease the ship into hyperdrive. The stars melt away. What took you so long?? A colleagues shouts as you land. We have been waiting for this load for time time now! If only you knew.... Well, it's doesnt matter now, save you story for another time. This will finally finish the raygun we made to destory the plant Earth, I suppose a congragulations is in order but we don't have the time for that right now. Dismissed.";
     } else {
-      output = "Time runs out, and the ship crumbles to pieces around you.";
+      output =
+        "Time runs out, the lights flicker one last time before you hear the ship power down...";
     }
     return (
       <div id="prompt">
