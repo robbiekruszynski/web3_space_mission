14c1e91a4cb89a58329ef4664b697a74b0b04fb2
robbie <robbiekruszynski@gmail.com>
2019-11-09 16:42:22 -0800

    add work to story page

--- src/Plot.js
+++ src/Plot.js
@@ -1,5 +1,5 @@
 export const plot = [
-  "Room 1 Plot",
+  "Room 1 Plot this is a test to check render",
   "Room 2 Plot",
   "Room 3 Plot",
   "Room 4 Plot",
--- src/Splash.js
+++ src/Splash.js
@@ -2,23 +2,37 @@ import React from "react";
 import Body from "./Body";
 
 class Splash extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      enterDiv: <div className="button"><h1>Story</h1><p>Background</p><button onClick={() => this.clickCheck()} className="animated fadeIn delay-2s">Continue</button></div>
-    }
+      enterDiv: (
+        <div className="button">
+          <h4 className="introStory">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </h4>
+
+          <button
+            onClick={() => this.clickCheck()}
+            className="animated fadeIn delay-2s"
+          >
+            Continue
+          </button>
+        </div>
+      )
+    };
   }
 
   clickCheck() {
-    this.setState({enterDiv:<Body>Loading...</Body>});
+    this.setState({ enterDiv: <Body>Loading...</Body> });
   }
   render() {
-    return (
-      <div>
-      {this.state.enterDiv}
-      </div>
-    );
+    return <div className="openBody">{this.state.enterDiv}</div>;
   }
 }
 
