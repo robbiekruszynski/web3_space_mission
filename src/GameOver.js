ef8dbef7f6521132fccaca39821e97fabf9c931b
Travis Toal <travisty12@gmail.com>
2019-11-17 01:36:26 -0800

    remove unnecessary escapes

--- src/GameOver.js
+++ src/GameOver.js
@@ -5,7 +5,7 @@ function GameOver(props) {
   if (props.happyEnd) {
     output =
       `The engines rumble, and you ease the ship into hyperdrive. The stars melt away.\n
-      An incoming message comes over your receiver as soon as you land. \"What took you so long??\" You hurry to hail them. \"We've been waiting for this load for some time now!\" You begin to retell your journey, but are cut off short. \"If only you knew.... Well, it doesnt matter now, save your story for another time. This will finally finish the raygun we made to destroy the invader's home planet. I suppose a congratulations is in order, but we don't have the time for that right now. First, we must prepare to atomize this 'Earth'. Dismissed.\"`
+      An incoming message comes over your receiver as soon as you land. "What took you so long??" You hurry to hail them. "We've been waiting for this load for some time now!" You begin to retell your journey, but are cut off short. "If only you knew.... Well, it doesnt matter now, save your story for another time. This will finally finish the raygun we made to destroy the invader's home planet. I suppose a congratulations is in order, but we don't have the time for that right now. First, we must prepare to atomize this 'Earth'. Dismissed."`
   } else {
     output =
       "Time runs out, the lights flicker one last time before you hear the ship power down...";
